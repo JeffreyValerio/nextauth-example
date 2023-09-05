c03547b66d8b9490ee3d4197e2afeadf9e3ba227
JeffreyValerio <jeffreyvalerio@hotmail.com>
2023-09-05 05:49:56 -0600

    edited

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,13 +2,8 @@ import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 
-interface userSession {
-    session: any,
-    token: any,
-    user: any
-}
-
 export const handler = NextAuth({
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID as string,
@@ -19,19 +14,14 @@ export const handler = NextAuth({
             clientSecret: process.env.GOOGLE_SECRET as string
         })
     ],
-    secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
         async jwt({ token, account, profile, trigger, isNewUser, session }: any) {
-
-            token.accessToken = account.id_token
-
             console.log({ token, account, profile, trigger, isNewUser, session })
             return token
         },
 
-        async session({ session, token, user }: userSession) {
+        async session({ session, token, user }: any) {
             session.access_token = token.accessToken
-            session.user = token.user as any
             console.log({ session, token, user })
             return session
         }
